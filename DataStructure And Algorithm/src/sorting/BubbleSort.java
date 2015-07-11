f9b8a9b41f0ae6dd362b58c97bb7a3c47cc6c4cf
ipsitpatra <ipsit.patra@gmail.com>
2015-07-10 21:01:43 -0500

    Bubble Sort

--- DataStructure And Algorithm/src/sorting/BubbleSort.java
+++ DataStructure And Algorithm/src/sorting/BubbleSort.java
@@ -3,10 +3,44 @@
  */
 package sorting;
 
+
 /**
  * @author Ipsit
  *
  */
 public class BubbleSort {
 
+	public int[] bubbleSort(int[] list){
+
+		int size = list.length;
+		boolean flagChange = false;
+		int tmp;
+		while(size!=1){
+			for (int i = 0; i < size-1; i++) {
+				if(list[i]>list[i+1]){
+					tmp = list[i];
+					list[i]=list[i+1];
+					list[i+1] = tmp;
+					flagChange =true;
+				}
+			}
+			if(!flagChange)
+				break;
+			size--;
+			flagChange =false;
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+
+//		int[] a= {7,6,5,4,3,2,1};
+		int[] a= {2,1,3,4,5,6,7};
+		BubbleSort b = new BubbleSort();
+		b.bubbleSort(a);
+		
+		for(int c:a)
+			System.out.print(c+" ");
+
+	}
 }
