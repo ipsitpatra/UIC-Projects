b39eed7bd6868b904aa3e8b250dcb4f41e064594
Ipsit Patra <ipatra2@uic.edu>
2015-09-30 22:07:26 -0500

    String Intersection

---
+++ DataStructure And Algorithm/src/javaProblem/StringIntersection.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package javaProblem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * @author Ipsit
+ *
+ */
+public class StringIntersection {
+	public static void main(String[] args) {
+
+		String s1 = "sycheyllesysy";  
+		String s2 = "sydneye";  
+		ArrayList<Character> h1 = new ArrayList<Character>(), h2 = new ArrayList<Character>(), h3 = new ArrayList<Character>();
+		for(int i = 0; i < s1.length(); i++)                                            
+			h1.add(s1.charAt(i));
+		for(int i = 0; i < s2.length(); i++)
+			h2.add(s2.charAt(i));
+		int size = h1.size() > h2.size() ? h1.size() : h2.size();
+		for (int i = 0; i < size; i++) {
+			if(h2.remove(h1.get(i))){
+				h3.add(h1.get(i));
+//				h1.remove(i);
+//				size--;
+//				i--;
+			}
+		}		
+
+		Character[] res = h3.toArray(new Character[0]);
+		
+		Arrays.sort(res);
+		for (int i = 0; i < res.length; i++) {
+			System.out.print(res[i]);
+		}
+
+
+	}
+}
